933b06ca53a7785bf07f7ab6035add6483cff20f
Michael Shervashidze <mike.fury315@gmail.com>
2025-04-10 15:26:38 +0700

    add mask on phone

--- app.js
+++ app.js
@@ -7,7 +7,68 @@ const popupContent = popupConsult.querySelector(".popup__content");
 const form = popupConsult.querySelector(".popup__form");
 const popupMessage = popupConsult.querySelector('.popup__message');
 const body = document.body;
+const phoneInput = document.getElementById('phone');
 
+const inputMask = () => {
+    if (!this.value) {
+        this.value = '+7 (';
+    }
+}
+
+const inputMaskFormat = (event) => {
+    // Сохраняем текущее значение и позицию курсора
+    let oldValue = event.target.value;
+    let cursorPosition = event.target.selectionStart;
+
+    // Очищаем значение от всех нецифровых символов
+    let value = event.target.value.replace(/\D/g, '');
+
+    // Если начинается с '7', убираем её, так как +7 уже есть в маске
+    if (value.startsWith('7')) {
+        value = value.substring(1);
+    }
+
+    // Ограничиваем ввод до 10 цифр
+    value = value.substring(0, 10);
+
+    // Форматируем значение по шаблону
+    let formatted = '+7 (';
+    if (value.length > 0) {
+        formatted += value.substring(0, 3);
+    }
+    if (value.length >= 3) {
+        formatted += ') ' + value.substring(3, 6);
+    }
+    if (value.length >= 6) {
+        formatted += '-' + value.substring(6, 8);
+    }
+    if (value.length >= 8) {
+        formatted += '-' + value.substring(8, 10);
+    }
+
+    // Устанавливаем отформатированное значение
+    event.target.value = formatted;
+
+    // Вычисляем новую позицию курсора
+    let digitsBeforeCursor = oldValue.substring(0, cursorPosition).replace(/\D/g, '').length;
+    let newCursorPosition = 0;
+    let digitsCount = 0;
+
+    for (let i = 0; i < formatted.length; i++) {
+        if (/\d/.test(formatted[i])) {
+            digitsCount++;
+            if (digitsCount === digitsBeforeCursor) {
+                newCursorPosition = i + 1;
+                break;
+            }
+        } else {
+            newCursorPosition = i + 1;
+        }
+    }
+
+    // Устанавливаем курсор в правильную позицию
+    event.target.setSelectionRange(newCursorPosition, newCursorPosition);
+}
 
 const centerPopup = () => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop; // Current scroll position
@@ -31,6 +92,8 @@ const openPopup = () => {
     popupOverlay.addEventListener("click", closePopup);
     popupButtonClose.addEventListener("click", closePopup);
     popupMessageButtonClose.addEventListener("click", closePopup);
+    phoneInput.addEventListener('focus', inputMask);
+    phoneInput.addEventListener('input', inputMaskFormat);
     centerPopup();
 };
 
@@ -43,6 +106,8 @@ const closePopup = () => {
     popupOverlay.removeEventListener("click", closePopup);
     popupButtonClose.removeEventListener("click", closePopup);
     popupMessageButtonClose.removeEventListener("click", closePopup);
+    phoneInput.removeEventListener('focus', inputMask);
+    phoneInput.removeEventListener('input', inputMaskFormat);
     form.reset();
 };
 
